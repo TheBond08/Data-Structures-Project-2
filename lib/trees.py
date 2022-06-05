ba943fb850202666077beb80dec4d9397d6f8d9d
Panos-Kouris <pakou2003@gmail.com>
2022-06-05 17:04:39 +0300

    1) Διαγραφη Max και Last 2) Αναγνωση CSV

--- lib/trees.py
+++ lib/trees.py
@@ -1,47 +1,46 @@
 import csv
 
 file = open("MOCK_DATA.csv", newline='')
-    reader = csv.reader(file)
+reader = csv.reader(file)
 
-    header = next(reader)
+header = next(reader)
 
-    # Πινακας αποθήκευσης δεδομένων csv
-    dataRecord = []
-    # Τροποποίηση δεδομένων απο str σε ο,τι επιθημούμε
-    for row in reader:
-        id = int(row[0])
-        firstname = str(row[1])
-        lastname = str(row[2])
-        email = str(row[3])
+# Πινακας αποθήκευσης δεδομένων csv
+dataRecord = []
+# Τροποποίηση δεδομένων απο str σε ο,τι επιθημούμε
+for row in reader:
+    id = int(row[0])
+    firstname = str(row[1])
+    lastname = str(row[2])
+    email = str(row[3])
 
-        dataRecord.append([id, firstname, lastname, email])
-
-
-
+    dataRecord.append([id, firstname, lastname, email])
 
 class node:
     def __init__(self):
-        self.arithmos_mitroou = ""
-        self.onoma = ""
-        self.eponimo= ""
-        self.b = 0.0
-        self.ap = 0
+        self.id = 0
+        self.firstname = ""
+        self.lastname= ""
+        self.email = " "
         self.left = None
         self.right = None
 
+
+
+
 def createNode():
     r = node()
-    textLine = input("Give data:")
-    dataRecord = textLine.strip().split()
-    r.arithmos_mitroou = dataRecord[0]
-    r.onoma = dataRecord[1]
-    r.eponimo = dataRecord[2]
-    r.b = float(dataRecord[3])
-    r.ap = int(dataRecord[4])
+
+
+    r.id = dataRecord[0]
+    r.firstname = dataRecord[1]
+    r.lastname= dataRecord[2]
+    r.email = dataRecord[3]
+
     return r
 
 def addInOrder(root, newNode):
-    if newNode.arithmos_mitroou <= root.arithmos_mitroou:
+    if newNode.id <= root.id:
         if root.left == None:
             root.left = newNode
         else:
@@ -55,7 +54,7 @@ def addInOrder(root, newNode):
 def showInOrder(root):
     if root != None:
         showInOrder(root.left)
-        print(("%s %s %s %.2f %d") % (root.arithmos_mitroou, root.on, root.eponimo, root.b, root.ap))
+        print((root.id, root.firstname, root.lastname, root.email))
         showInOrder(root.right)
 
 def countNodes(root):
@@ -66,37 +65,19 @@ def countNodes(root):
         result += countNodes(root.right)
     return result
 
-def findByarithmos_mitroou(root, farithmos_mitroou):
+def findByID(root, id):
     result = None
 
     if root != None:
-        if farithmos_mitroou < root.arithmos_mitroou :
-            result = findByarithmos_mitroou(root.left, farithmos_mitroou)
-        elif farithmos_mitroou == root.arithmos_mitroou :
+        if id < root.id :
+            result = findByID(root.left, id)
+        elif id == root.id :
             result = root
         else:
-            result = findByarithmos_mitroou(root.right, farithmos_mitroou)
+            result = findByID(root.right, id)
 
     return result
 
-def maxarithmos_mitroou(root) :
-    if root.right == None:
-        result = root
-    else:
-        result = maxarithmos_mitroou(root.right)
-    return result
-
-def maxGrade(root):
-    result = -1
-
-    if root != None:
-        result = maxGrade(root.left)
-        if result < root.b:
-            result = root.b
-        resultRight = maxGrade(root.right)
-        if result < resultRight:
-            result = resultRight
-    return result
 
 root = None
 
@@ -104,9 +85,7 @@ while True:
     print("""-------------------Menu-----------------------")
 1. Add in order
 2. Show in order
-3. Find student by arithmos_mitroou
-4. Show last student by arithmos_mitroou
-5. Show maximum grade
+3. Find student by ID
 0. Exit""")
     
     ch = input("Choice: ").strip()
@@ -123,24 +102,13 @@ while True:
         showInOrder(root)
         print(("Number of node:%d") % (countNodes(root)))
     elif ch == "3":
-        userarithmos_mitroou = input("Give arithmos_mitroou: ")
-        temp = findByarithmos_mitroou(root, userarithmos_mitroou)
+        userarithmos_mitroou = input("Give ID: ")
+        temp = findByID(root, userarithmos_mitroou)
         if temp == None:
             print("Not found!")
         else:
-            print(("%s %s %s %.2f %d") % (temp.arithmos_mitroou, temp.on, temp.eponimo,
-                                          temp.b, temp.ap))
-    elif ch == "4":
-        if root == None:
-            print("Empty tree!")
-        else:
-            temp = maxarithmos_mitroou(root)
-            print(("%s %s %s %.2f %d") % (temp.arithmos_mitroou, temp.on, temp.eponimo,
-                                          temp.b, temp.ap))
-    elif ch == "5":
-        if root == None:
-            print("Empty tree!")
-        else:
-            print(("Max grade: %.2f") % (maxGrade(root)))
+            print(("%d %s %s %s") % (temp.id, temp.firstname, temp.lastname,
+                                          temp.email))
+
     else:
         print("Try again!")
