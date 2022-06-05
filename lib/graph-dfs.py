c669329ee96ed09292bc89f21b821705c6996c0c
Dr Efficient <67394745+sudo455@users.noreply.github.com>
2022-06-05 14:36:55 +0300

    simplify trees bfs and dfs aka more readable

--- lib/graph-dfs.py
+++ lib/graph-dfs.py
@@ -1,4 +1,13 @@
-import os
+def dfs(k):
+    Mark[k] = 1
+    print("Now in ", (k+1))
+    for j in range(len(a)):
+        if a[k][j] == 1 and Mark[j] == 0:
+            #print(a[k][j] == 1, " and ", Mark[j] == 0, " = ", a[k][j] == 1 and Mark[j] == 0)
+            #print(Mark)
+            dfs(j)
+    return
+
 a = [[0, 1, 1, 1, 0, 0, 0, 0, 0, 0],	
      [1, 0, 0, 0, 1, 1, 0, 0, 0, 0], 
      [1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
@@ -11,22 +20,13 @@ a = [[0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
      [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]]
 
 Mark = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-N = len(a)
 
-def dfs(k):
-    Mark[k] = 1
-    print("Now in %d" % (k+1))
-    for j in range(N):
-        if a[k][j] == 1 and Mark[j] == 0:
-            dfs(j)
-
-    return
-
-p = input("Give start node (1-%d) or out of range to exit: " % (N))
-i = int(p)
-while i >= 1 and i <= N:
-    for m in range(N):
-        Mark[m] = 0
-    dfs(i-1)
-    p = input("Give start node (1-%d) or out of range to exit: " % (N))
-    i = int(p)
+while True:
+    
+    i = int(input("Give start node (1-%d) or out of range to exit: " % (len(a))))
+    if i >= 1 and i <= len(a):
+        for m in range(len(a)):
+            Mark[m] = 0
+        dfs(i-1)
+    else:
+        break
