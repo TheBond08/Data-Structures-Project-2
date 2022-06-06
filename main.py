0c31da93c186662d8a5f7983e4a19d6fe0bdacc9
Dr Efficient <67394745+sudo455@users.noreply.github.com>
2022-06-06 20:38:10 +0300

    simple print fix

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from lib.trees import trees
 from lib.bst import binary_search_tree
 
 while True:
-    print("""
+    print("""Welcome to the project:
+--------------------MENU--------------------
         1- binary search tree
         2- trees""")
     user_input = int(input("give me 1 or 2 or out of range to quit: "))
